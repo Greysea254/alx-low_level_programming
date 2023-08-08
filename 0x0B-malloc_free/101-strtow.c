67c5f896b8d3d97f18bd1d8f8ff89b7e9129bc08
Grace <gwacera4@gmail.com>
2023-08-08 00:57:50 -0700

    Write a function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,76 +1,75 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 
-/**
- * count_word - helper function to count the number of words in a string
- * @s: string to evaluate
- *
- * Return: number of words
- */
-int count_word(char *s)
-{
-	int flag, c, w;
+char **strtow(char *str) {
+    if (str == NULL || *str == '\0') {
+        return NULL; // Return NULL for invalid input
+    }
 
-	flag = 0;
-	w = 0;
+    // Count the number of words first
+    int word_count = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            word_count++;
+            while (str[i] != ' ' && str[i] != '\0') {
+                i++;
+            }
+        }
+    }
 
-	for (c = 0; s[c] != '\0'; c++)
-	{
-		if (s[c] == ' ')
-			flag = 0;
-		else if (flag == 0)
-		{
-			flag = 1;
-			w++;
-		}
-	}
+    // Allocate memory for an array of strings (words) and NULL terminator
+    char **words = (char **)malloc((word_count + 1) * sizeof(char *));
+    if (words == NULL) {
+        return NULL; // Return NULL if memory allocation fails
+    }
 
-	return (w);
+    int word_index = 0;
+    int word_start = 0;
+    
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            word_start = i;
+            while (str[i] != ' ' && str[i] != '\0') {
+                i++;
+            }
+            
+            int word_length = i - word_start;
+            words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
+            if (words[word_index] == NULL) {
+                // Free memory allocated so far and return NULL if memory allocation fails
+                for (int j = 0; j < word_index; j++) {
+                    free(words[j]);
+                }
+                free(words);
+                return NULL;
+            }
+            
+            strncpy(words[word_index], str + word_start, word_length);
+            words[word_index][word_length] = '\0';
+            word_index++;
+        }
+    }
+    
+    words[word_count] = NULL; // Set the last element to NULL
+    
+    return words;
 }
-/**
- * **strtow - splits a string into words
- * @str: string to split
- *
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)
- */
-char **strtow(char *str)
-{
-	char **matrix, *tmp;
-	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	while (*(str + len))
-		len++;
-	words = count_word(str);
-	if (words == 0)
-		return (NULL);
+int main() {
+    char str[] = "This is a test string";
+    char **words = strtow(str);
 
-	matrix = (char **) malloc(sizeof(char *) * (words + 1));
-	if (matrix == NULL)
-		return (NULL);
+    if (words != NULL) {
+        for (int i = 0; words[i] != NULL; i++) {
+            printf("Word %d: %s\n", i, words[i]);
+            free(words[i]); // Free memory for each word
+        }
+        free(words); // Free memory for the array of words
+    } else {
+        printf("Memory allocation failed.\n");
+    }
 
-	for (i = 0; i <= len; i++)
-	{
-		if (str[i] == ' ' || str[i] == '\0')
-		{
-			if (c)
-			{
-				end = i;
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
-				if (tmp == NULL)
-					return (NULL);
-				while (start < end)
-					*tmp++ = str[start++];
-				*tmp = '\0';
-				matrix[k] = tmp - c;
-				k++;
-				c = 0;
-			}
-		}
-		else if (c++ == 0)
-			start = i;
-	}
-
-	matrix[k] = NULL;
-
-	return (matrix);
+    return 0;
+}
